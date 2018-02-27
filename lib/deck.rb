ef69db2611d00606da9f36cd9bb7a0fafb9d2af1
SelamawitA <Selamawit.ainalem@gmail.com>
2018-02-26 22:38:30 -0800

    Created a Deck class with a cards(array) attribute.Deck class contains 52 card objects, can shuffle, deal a card, and return the current number of cards.

--- lib/deck.rb
+++ lib/deck.rb
@@ -2,18 +2,40 @@
 # deck.rb
 
 require_relative 'card'
-
+NUM_OF_CARDS_PER_SUIT = 13
 class Deck
-
+  attr_reader :cards
   def initialize
-
+    @cards = []
   end
+
+  def create_deck
+    suits = [:hearts, :spades, :clubs, :diamonds]
+    suits.each do |a_suit|
+      count = 0
+      NUM_OF_CARDS_PER_SUIT.times do
+        count+=1
+        self.cards << Card.new(count,a_suit)
+      end
+    end
+  end
+  
 
   def draw
-    # returns a card
+    if cards.length == 0
+      raise ArgumentError.new 'Cards have all been pulled, create new deck.'
+    end
+    return cards.pop
   end
 
+
   def shuffle
-    # shuffles the deck
+    return cards.shuffle!
   end
+
+  def number_of_cards_in_deck
+    return cards.length
+  end
+
+
 end
